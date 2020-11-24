b45d5ec2edbfdfec0ee3b7f7abc6f7a8182edb35
Carlos Quintero <carlosquintero95nic@gmail.com>
2020-11-24 16:15:21 -0600

    {Upgrade on Components  and } Agregamos lógica de programación a este componente y realizamos ligeros cambios en el css para mejor performance en la transición del switch

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/components/Switch.css';
 
 const Switch = () => {
+  const [ playmentPlan, setPlaymentPlan ] = useState('Annually');
+  const [ switchStyles, setSwitchStyles ] = useState('switch');
+
+  const handleChangePlaymentPlan = () => {
+    if (playmentPlan === 'Annually') {
+      setPlaymentPlan('Monthly');
+      setSwitchStyles('switch monthly')
+    } else {
+      setPlaymentPlan('Annually');
+      setSwitchStyles('switch')
+    }
+  };
+
   return(
     <div className="conten_plans">
       <p className="copy_switch">Annually</p>
-      <button className="switch" id="switch"></button>
+      <button
+        onClick={handleChangePlaymentPlan}
+        className={switchStyles}>
+      </button>
       <p className="copy_switch">Monthly</p>
     </div>
   );
